7577f0d61d77c0b067ae7da6846d3e97924758be
mahalakshmi-005 <mahalakshmidd744@gmail.com>
2024-12-11 12:18:52 +0530

    Initial Commit

---
+++ SmileDesk/SmileDesk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmileDesk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
